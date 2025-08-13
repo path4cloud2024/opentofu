e01851b2673a1599f7f5c253209bf70eceede7a9
amitaryan0010 <amitaryan0010@gmail.com>
2025-08-14 02:40:28 +0530

    day 2 code

---
+++ 02_multi_provider.tf
@@ -0,0 +1,10 @@
+resource "local_file" "file2"{
+    filename = "E:/Path4Cloud Trainings/OpenTofu/demo/multi_file.txt"
+    content = "Welcome to path4cloud, we are learning multi providers."
+}
+
+resource random_pet my_fav {
+  length    = 2
+  prefix    = "master"
+  separator = "."
+}
