b1ea8e5e55e429afd4c04ce5415a69d8d2083f66
amitaryan0010 <amitaryan0010@gmail.com>
2025-08-18 22:22:02 +0530

    added resources

---
+++ 05_res_dependency.tf
@@ -0,0 +1,26 @@
+resource local_file depend_file {
+    filename = "E:/Path4Cloud Trainings/OpenTofu/demo/depend.txt"
+    content = "Here we are learning explicit dependency"
+
+    depends_on = [
+        random_pet.my_pet
+    ]
+}
+
+resource random_pet my_pet {
+    prefix = "master"
+    length = 2
+    separator = "-"
+}
+
+output "my_pet" {
+    value = random_pet.my_pet.id
+}
+
+resource "time_static" time{
+
+}
+
+output "cur_time" {
+    value = time_static.time.id
+}
